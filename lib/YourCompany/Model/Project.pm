0fd78df1016ea10741b209a0b059de5c7fd28a70
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2017-04-22 21:13:49 +0300

    Anything should be explicitly wrapped by transaction.

--- lib/YourCompany/Model/Project.pm
+++ lib/YourCompany/Model/Project.pm
@@ -10,21 +10,25 @@ use YourCompany::Plack::Error;
 use parent qw( YourCompany::Model );
 
 sub list {
-    return [
-        YourCompany::DB->rs('Project')->all,
-    ];
+    return YourCompany::DB->txn_do(sub {
+        return [
+            YourCompany::DB->rs('Project')->all,
+        ];
+    });
 }
 
 sub single {
     my ( $self, $id ) = @_;
-    my $project = YourCompany::DB->rs('Project')->search({
-        id => $id,
-    })->single;
+    return YourCompany::DB->txn_do(sub {
+        my $project = YourCompany::DB->rs('Project')->search({
+            id => $id,
+        })->single;
 
-    YourCompany::Plack::Error->throw( HTTP_NOT_FOUND, "Project not found: $id" )
-        unless $project;
+        YourCompany::Plack::Error->throw( HTTP_NOT_FOUND, "Project not found: $id" )
+            unless $project;
 
-    return $project;
+        return $project;
+    });
 }
 
 1;
