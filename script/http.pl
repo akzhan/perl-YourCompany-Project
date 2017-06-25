9ac6cb6a1ccb873b2b265b3909ab2f6f7048c4e4
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2017-06-25 14:38:48 +0300

    Add session management.

--- script/http.pl
+++ script/http.pl
@@ -2,9 +2,21 @@ use YourCompany::Perl::UTF8;
 
 use Plack::Handler::Starman;
 use Plack::Builder;
+use Plack::Session::State::Cookie;
+use Plack::Session::Store::RedisFast;
+
+use YourCompany::Util::Redis ();
 
 my $app = Plack::Util::load_psgi "./script/cli";
 
 builder {
+    enable 'Session',
+      state => Plack::Session::State::Cookie->new(
+        expires => YourCompany::Config->app->{session}{expires} ),
+      store => Plack::Session::Store::RedisFast->new(
+        redis   => YourCompany::Util::Redis->session,
+        expires => YourCompany::Config->app->{session}{expires},
+      );
+
     $app;
 };
